0ea058b1224bf97362e12b011e78a68de73096c9
kaveendra96 <kaveendra96@gmail.com>
2020-02-17 07:32:29 +0530

    vehicle completee

--- src/main/java/lk/cmb/ucsc/drivingschool/repo/VehicleRepo.java
+++ src/main/java/lk/cmb/ucsc/drivingschool/repo/VehicleRepo.java
@@ -2,7 +2,9 @@ package lk.cmb.ucsc.drivingschool.repo;
 
 import lk.cmb.ucsc.drivingschool.model.Vehicle;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepo extends MongoRepository<Vehicle,String> {
 
 }
